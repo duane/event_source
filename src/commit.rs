538c656a3ad72760a66afa9448cb73b84c79f40d
Duane Bailey <bailey.d.r@gmail.com>
2020-09-14 01:50:10 -0600

    Added mild commit test.

--- src/commit.rs
+++ src/commit.rs
@@ -59,3 +59,50 @@ impl Commit {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::Commit;
+  use uuid::Uuid;
+  use chrono::Utc;
+  #[test]
+  fn deserialize() {
+    let serialized_events = "[{\"foo\": \"bar\"}, {\"baz\": \"bat\"}]".as_bytes().to_vec();
+    let serialized_metadata = "[{\"foo2\": \"bar2\", \"baz2\": \"bat2\"}]".as_bytes().to_vec();
+    let commit = Commit{
+      aggregate_id: Uuid::new_v4(),
+      aggregate_version: 18,
+      commit_id: Uuid::new_v4(),
+      commit_sequence: 101,
+      commit_number: 198,
+      commit_timestamp: Utc::now(),
+      serialized_events,
+      serialized_metadata,
+      events_count: 4,
+      dispatched: true,
+    };
+
+    let deserialized = commit.deserialize();
+
+    assert_eq!(deserialized.aggregate_id, commit.aggregate_id);
+    assert_eq!(deserialized.aggregate_version, commit.aggregate_version);
+    assert_eq!(deserialized.commit_id, commit.commit_id);
+    assert_eq!(deserialized.commit_sequence, commit.commit_sequence);
+    assert_eq!(deserialized.commit_number, commit.commit_number);
+    assert_eq!(deserialized.commit_timestamp, commit.commit_timestamp);
+    assert_eq!(deserialized.events_count, commit.events_count);
+    assert_eq!(deserialized.dispatched, commit.dispatched);
+
+    let events_array = deserialized.events.as_array().unwrap();
+    assert_eq!(events_array.len(), 2);
+    assert_eq!(events_array[0].as_object().unwrap()["foo"], "bar");
+    assert_eq!(events_array[1].as_object().unwrap()["baz"], "bat");
+
+    let metadata_obj = deserialized.metadata.as_array().unwrap()[0].as_object().unwrap();
+    assert_eq!(metadata_obj.len(), 2);
+    assert_eq!(metadata_obj["foo2"], "bar2");
+    assert_eq!(metadata_obj["baz2"], "bat2");
+    assert_eq!(events_array[0].as_object().unwrap()["foo"], "bar");
+  }
+
+}
